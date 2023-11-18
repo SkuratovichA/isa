1c7fe10bcaab632cfa7c935ef7313e195c06a3eb
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2023-11-18 16:42:15 +0100

    add trycatch when constructing a packet && add std::endl

--- main.cpp
+++ main.cpp
@@ -5,24 +5,30 @@
 
 #include <iostream>
 
+
 int main(int argc, const char** argv) {
     DNSConfiguration args{};
     try {
         args = argparser::parseArguments(argc, argv);
     } catch (const std::system_error &err) {
-        std::cerr << err.what();
+        std::cerr << err.what() << std::endl;
         return -1;
     }
 
     dns::Packet queryPacket;
     dns::Server server;
-    tie(queryPacket, server) = dns::constructQueryPacket(args);
+    try {
+        tie(queryPacket, server) = dns::constructQueryPacket(args);
+    } catch (const std::system_error &err) {
+        std::cerr << err.what() << std::endl;
+        return -1;
+    }
 
     std::vector<uint8_t> response;
     try {
-        response = udp::sendQuery(server.address, server.port, queryPacket);
+        response = udp::sendQuery(server.address, server.port, args.address, queryPacket);
     } catch (std::system_error &err) {
-        std::cerr << err.what();
+        std::cerr << err.what() << std::endl;
         return -1;
     }
 
